b7b4eed61f47d270eefe1cc9651e1352a48d7a1f
Ruslan Hladchenko <rugladchenko@gmail.com>
2023-10-27 09:08:24 +0300

    Change main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import {Button, TextField} from "@mui/material";
 
 function App() {
     return (
         <>
-            <h1>Shortener</h1>
+            <div style={{display: "flex", justifyContent: "center"}}>
+                <div style={{display: "flex", justifyContent: "center", flexDirection: "column"}}>
+                    <h1>Shortener</h1>
+                    <TextField id="shortUrl" label="Short URL"/>
+                    <Button variant="text">Create</Button>
+                </div>
+            </div>
         </>
     );
 }
